51d796118d6518c8e8d719906b25104ae30fd410
tberriel <57666961+tberriel@users.noreply.github.com>
2025-05-23 22:55:11 +0200

    MAJOR FIX: * Update G2O to 20170730 to fix compatibility with Eigen3.4 . Detailed changes are in g2o/README.txt. * Examples executables are back in ./Examples rather than in ./build/bin/Examples

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
-install_path=$1
-
+if [ $# -eq 0 ]
+then
+  install_path=$CONDA_PREFIX
+else
+  install_path=$1
+fi
+BUILD_MODE="Release"
 echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
 
 cd Thirdparty/DBoW2
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release 
+cmake .. -DCMAKE_BUILD_TYPE="Release"
 make -j
 cd ../../
 
@@ -12,20 +21,17 @@ cd g2o
 echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release 
-make -j
+cmake .. -DCMAKE_INSTALL_PREFIX="../install" \
+         -DCMAKE_BUILD_TYPE="Release" 
+make install -j
 
 cd ../../
 cd ../
 echo "Uncompress vocabulary ..."
 
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ..
-
 echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_MODE  \
          -DCMAKE_INSTALL_PREFIX=${install_path}
 make install -j
